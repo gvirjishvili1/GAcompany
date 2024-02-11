d7a46fa58b4949f5b65ea49151be01958eac0683
avtandil gvirjishvili <gvirjishviliavtandil1@gmail.com>
2024-02-11 05:42:15 +0400

    Some Changes

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-var searchButton = $("#searchBtn");
-var slide = $("#carousel")
-var searchBar = $(".search")
 
-searchButton.on("click", function() {
-    searchBar.toggle();
-})
+$(document).ready(function () {
+     const searchButton = $("#searchBtn");
+     const searchBar = $(".search");
+     const $carousel = $('.carousel');
+     const $carouselItems = $('.carousel-item');
+     const $nextButton = $('#next-button');
+     const $prevButton = $('#prev-button');
+   
+     let currentIndex = 0;
+     $(searchBar).hide();
+     $(searchButton).click(function(){
+         $(searchBar).toggle();
+     }); 
+     
+     updateCarousel();
+     
+     function updateCarousel() {
+          const translateValue = `${-currentIndex * 50}%`;
+          $carousel.css('transform', `translateX(${translateValue})`);
+     }
+
+     function nextSlide() {
+          console.log('Next slide clicked');
+          currentIndex = (currentIndex + 1) % $carouselItems.length;
+          updateCarousel();
+     }
+      
+      function prevSlide() {
+          console.log('Prev slide clicked');
+          currentIndex = (currentIndex - 1 + $carouselItems.length) % $carouselItems.length;
+          updateCarousel();
+     }
+     
+     $nextButton.on('click', nextSlide);
+     $prevButton.on('click', prevSlide);
+});
+
