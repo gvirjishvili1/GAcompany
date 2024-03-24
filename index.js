4f2e96a919cbeca68adf612f20606c103585a151
gvirjishvili1 <124874947+gvirjishvili1@users.noreply.github.com>
2024-03-24 20:17:04 +0400

    searchBar add

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
 $(document).ready(function () {
-     const searchButton = $("#searchBtn");
-     const searchBar = $(".search");
-     const $carousel = $('.carousel');
-     const $carouselItems = $('.carousel-item');
-     const $nextButton = $('#next-button');
-     const $prevButton = $('#prev-button');
-   
-     let currentIndex = 0;
+    const $carousel = $('.carousel');
+    const $carouselItems = $('.carousel-item');
+    const $nextButton = $('#next-button');
+    const $prevButton = $('#prev-button');
 
-     $("loading-overlay").fadeOut();
+    let currentIndex = 0;
 
-     $(searchBar).hide();
-     
-     $(searchButton).click(function(){
-         $(searchBar).toggle();
-     }); 
-     
+    // Corrected selector for loading overlay
+    $(".loading-overlay").fadeOut();
 
-     updateCarousel();
-     
-     function updateCarousel() {
-          const translateValue = `${-currentIndex * 50}%`;
-          $carousel.css('transform', `translateX(${translateValue})`);
-     }
+    updateCarousel();
 
-     function nextSlide() {
-          currentIndex = (currentIndex + 1) % $carouselItems.length;
-          updateCarousel();
-     }
+    function updateCarousel() {
+        const translateValue = `${-currentIndex * 50}%`;
+        $carousel.css('transform', `translateX(${translateValue})`);
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % $carouselItems.length;
+        updateCarousel();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + $carouselItems.length) % $carouselItems.length;
+        updateCarousel();
+    }
+
+    $nextButton.on('click', nextSlide);
+    $prevButton.on('click', prevSlide);
+
+    /* search bar */
+
+    $(document).ready(function() {
+        const nav = $("nav"),
+        searchIcon = $("#searchBtn"),
+        searchBar = $(".search-box input");
       
-      function prevSlide() {
-          currentIndex = (currentIndex - 1 + $carouselItems.length) % $carouselItems.length;
-          updateCarousel();
-     }
-     
-     $nextButton.on('click', nextSlide);
-     $prevButton.on('click', prevSlide);
+        searchIcon.on("click", function() {
+            nav.toggleClass("openSearch");
+            nav.removeClass("openNav");
+            if (nav.hasClass("openSearch")) {
+                searchIcon.removeClass("bi-search").addClass("bi-x");
+                $(".bi-x").css("font-size", "32px");
+                $(".navbar-links").css("display", "none");
+                searchBar.focus();
+            } else {
+                searchIcon.removeClass("bi-x").addClass("bi-search");
+                $(".bi-search").css("font-size", "20px");
+                $(".navbar-links").css("display", "flex");
+                searchBar.blur();
+            }
+        });
+      
+    });
 });
